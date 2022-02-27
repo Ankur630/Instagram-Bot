30b704791a7bf5a8c1e121cf360534ef751494f8
Ankur630 <100507265+Ankur630@users.noreply.github.com>
2022-02-27 15:05:54 +0530

    Update insta_cmds.py

--- plugins/insta_cmds.py
+++ plugins/insta_cmds.py
@@ -40,8 +40,8 @@ insta = Config.L
 buttons=InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-            InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
+            InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/ankur_pro'),
+            InlineKeyboardButton("🤖Other Bots", url="https://t.me/ankur_pro")
         ],
         [
             InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
