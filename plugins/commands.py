214cfedb5feae9798eba2d1a7a6db3fc34f62994
Ankur630 <100507265+Ankur630@users.noreply.github.com>
2022-02-27 15:01:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,8 +43,8 @@ async def start(bot, cmd):
 			reply_markup=InlineKeyboardMarkup(
 				[
 					[
-						InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-						InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
+						InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/ankur_pro'),
+						InlineKeyboardButton("🤖Other Bots", url="https://t.me/ankur_pro")
 					],
                     [
                         InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
